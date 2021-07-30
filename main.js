b6bfd0f7e6746c8b968614237a4f1dca38ed02c2
Maliksalmon2008 <83311416+Maliksalmon2008@users.noreply.github.com>
2021-07-30 13:17:17 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,31 +1,41 @@
 canvas = document.getElementById("myCanvas");
 ctx=canvas.getContext("2d");
-color="blue";
-ctx.beginPath();
-ctx.strokeStyle=color;
+ctx.beginPath(); 
+ctx.strokeStyle="blue";
 ctx.lineWidth=3;
 ctx.arc(200,200,40,0,2*Math.PI);
 ctx.stroke();
 
+ctx.beginPath();
+ctx.strokeStyle="black";
+ctx.lineWidth=3;
+ctx.arc(280,200,40,0,2*Math.PI);
+ctx.stroke(); 
+   
+ctx.beginPath();
+ctx.strokeStyle="red";
+ctx.lineWidth=3;
+ctx.arc(360,200,40,0,2*Math.PI);
+ctx.stroke(); 
+      
+ctx.beginPath();
+ctx.strokeStyle="yellow";
+ctx.lineWidth=3;
+ctx.arc(240,260,40,0,2*Math.PI);
+ctx.stroke(); 
 
-canvas.addEventListener("mousedown",mymousedown);
-
-function mymousedown(e){
-color=document.getElementById("color").value;
-console.log(color);
-mouse_X=e.clientX - canvas.offsetLeft;
-mouse_Y=e.clientY - canvas.offsetTop;
-circle(mouse_X,mouse_Y);
+ctx.beginPath();
+ctx.strokeStyle="green";
+ctx.lineWidth=3;
+ctx.arc(320,260,40,0,2*Math.PI);
+ctx.stroke(); 
+   
 
 
-}
-function circle(mouse_X,mouse_Y){
-    ctx.beginPath();
-    ctx.strokeStyle=color;
-    ctx.lineWidth=3;
-    ctx.arc(mouse_X,mouse_Y,40,0,2*Math.PI);
-    ctx.stroke(); 
-}
+
+
+
+
 function clear_area(){
     ctx.clearRect(0,0,canvas.width,canvas.height);
     
